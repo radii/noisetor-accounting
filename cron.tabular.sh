6d88ff2013ee617e362b5e9a631e5b93e1f6ff86
Michael C. Toren <mct@toren.net>
2012-04-10 00:51:57 -0700

    Try curl, rather than wget.

--- cron.tabular.sh
+++ cron.tabular.sh
@@ -5,5 +5,8 @@ URL=http://cha-ching.noisebridge.net/v1/donations/list/all/json/noisetor
 set -e
 cd $(dirname $0)
 git pull -q
-wget -T 60 -q -O noisetor.json $URL
+
+#wget -T 60 -q -O noisetor.json $URL
+curl -sS -f --connect-timeout 30 --max-time 60 -o noisetor.json $URL
+
 ./tabular.pl > finances.html
